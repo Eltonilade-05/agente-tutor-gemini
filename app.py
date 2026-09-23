2834b334b4b34e607fc4862ee80998faf2a6ffb8
Nunes Alfredo Assano <eltonmauride58@gmail.com>
2026-09-24 00:57:03 +0200

    atualilizacao do modelo

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ if prompt := st.chat_input("Digite sua dúvida ou cole um problema para estudarm
                 )
                 
                 resposta = client.models.generate_content(
-                    model="gemini-2.5-flash",
+                    model="gemini-1.5-flash",
                     contents=conteudos,
                     config=config
                 )
